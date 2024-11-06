1020baf81d8685b331cc9d0d3dae1d3aee0bd7ef
bzhao3927 <benz3927@gmail.com>
2024-11-06 16:32:58 -0500

    Update Moby_Dick.R

--- NLP/Moby_Dick.R
+++ NLP/Moby_Dick.R
@@ -1,6 +1,10 @@
 library(quanteda)
 require(readtext)
-dat_txtone <- readtext("moby.txt")
+
+# Read the text file from the URL
+url <- "https://raw.githubusercontent.com/benz3927/Probability-Seminar/refs/heads/main/NLP/moby.txt"
+dat_txtone <- readtext(url)
+
 toks <- tokens(tolower(dat_txtone$text), remove_punct = TRUE)|>
   tokens_split(separator = "—")
 ngrams <- tokens_ngrams( toks,n = 4,concatenator = "_")
@@ -18,3 +22,18 @@ B <- "ocean"
 prediction_text <- tokens_select(ngrams, pattern = phrase(paste0("*_", A)))
 y <- prediction_text[[1]]
 table(y)
+
+for (i in 1:length(y)){
+  find_phrase <- sub("}_[^_]+$}", ", y[i]")
+  occurances <- tokens_select(ngrams, pattern = phrase(find_phrase))
+  z <- table(occurances[[1]])
+  count <- z[y[i]]
+  prob <- count / length(z)
+  count2 <- z[paste0(find_phrase, "_", B)]
+  if (is.na(count2)){
+    count2 = 0
+  }
+  prob2 <- count2 / length(z)
+  probdif <- abs(prob - prob2)
+  dif <- dif + probdif
+}
