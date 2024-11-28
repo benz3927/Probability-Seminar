e6d78524f0084e72c1b32af3bee7f770fa188cdc
bzhao3927 <benz3927@gmail.com>
2024-11-28 09:36:13 -0500

    Update large_hospital_global.R

--- Schedule Optimization/large_hospital_global.R
+++ Schedule Optimization/large_hospital_global.R
@@ -33,7 +33,7 @@ nursestaffing <- c(12, 8)
 # Penalty 6: Weights for the stdev of nurses'/doctors' shifts in the schedule
 #            (trying to make everyone work similar amounts)
 #Penalty 7: Each instance of exceeding three night shifts/week
-penalties <- c(100, 100, 0.5, 5, 1, 2, 50)
+penalties <- c(100, 100, 0.5, 5, 1, 2, 10)
 
 # Number of iterations (if too small, might not be able to explore enough)
 niter <- 30000
@@ -82,7 +82,7 @@ get_energy_sub <- function(subsched, penalties) {
     
     # Penalize if worker exceeds the max number of night shifts allowed (penalize each night shift over the limit)
     if (night_shifts[i] > max_night_shifts_allowed) {
-      energy <- energy + penalties[6] * (night_shifts[i] - max_night_shifts_allowed)
+      energy <- energy + penalties[7] * (night_shifts[i] - max_night_shifts_allowed)
     }
     
     # Check the last shift and whether worker has the last day off
